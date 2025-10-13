01899762381eb8015949d00063a9f5c166601597
Ju-Min-Kyung <74389635+Ju-Min-Kyung@users.noreply.github.com>
2025-10-13 23:56:27 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 17.11 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,38 +1,49 @@
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        int[] answer = {0, 0};
+        int[] answer = {0,0};
         
-        int i = 0;
-        int j = 0;
-        int sum = sequence[0];
-        int min = Integer.MAX_VALUE;
+        int i = 0; // 앞
+        int j = 1;
+        int sum = sequence[i];
+        int min = 100000;
         int a = 0;
         int b = 0;
         
-        while (j < sequence.length) {
-            if (sum == k) {
-                if (min > j - i) {
-                    min = j - i;
-                    a = i;
-                    b = j;
-                }
-                sum -= sequence[i];
-                i++;
-            }
-            else if (sum > k) {
-                sum -= sequence[i];
-                i++;
-            }
-            else {
-                j++;
-                if (j < sequence.length) {
-                    sum += sequence[j];
-                }
+        for (int p = sequence.length-1; p >= k; p--) {
+            if (sequence[p] == k) {
+                answer[0] = p;
+                answer[1] = p;
+                return answer;
             }
         }
         
+        while (sequence[i] <= k / 2 && j < sequence.length) {
+            sum += sequence[j];
+            
+            if (sum == k) {
+    
+                if (min > (j - i)) {
+                    a = i;
+                    b = j;
+                    min = j - i;
+                 
+                    sum = sequence[i];
+                }
+                i++;
+                j = i+1;
+            }
+            else if (sum > k) {
+                i++;
+                j = i+1;
+                sum = sequence[i];
+            }
+            else {
+                j++;
+            }
+        }
         answer[0] = a;
         answer[1] = b;
+        
         return answer;
     }
 }
