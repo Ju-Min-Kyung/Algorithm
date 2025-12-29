9a7372ceb3bfde7fcded23c3d3cc26ca774b90e3
Ju-Min-Kyung <74389635+Ju-Min-Kyung@users.noreply.github.com>
2025-12-29 16:23:05 +0900

    [Bronze II] Title: 방 배정, Time: 104 ms, Memory: 14056 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws IOException
+	{
+	    int answer = 0;
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    
+	    // 학생 수 
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    int N = Integer.parseInt(st.nextToken());
+	    // 한 방에 묵을 수 있는 최대 인원
+	    int K = Integer.parseInt(st.nextToken());
+	    int[][] students = new int[6][2];
+	    
+	    for (int i = 0; i < N; i++) {
+	        StringTokenizer s = new StringTokenizer(br.readLine());
+	        int gender = Integer.parseInt(s.nextToken());
+	        int grade = Integer.parseInt(s.nextToken());
+	        students[grade-1][gender]++;
+	    }
+	    
+	    for (int i = 0; i < students.length; i++) {
+	        for (int j = 0; j < students[0].length; j++) {
+	            int rooms = (students[i][j] + 1) / 2;
+	            answer += rooms;
+	        }
+	    }
+	    
+	    System.out.println(answer);
+	}
+}
