0a1f636c21a4a849ad7d6535923031c407574552
Ju-Min-Kyung <74389635+Ju-Min-Kyung@users.noreply.github.com>
2026-01-10 16:34:35 +0900

    [Silver II] Title: 종이의 개수, Time: 908 ms, Memory: 311284 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+    static int number;
+    static int[][] map;
+    static int a, b, c;
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        calculator(0, 0, N);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+    }
+
+    public static void calculator(int x, int y, int n) {
+        if (n == 0) {
+            return;
+        }
+        boolean isAvailable = true;
+        // 모두 같은 숫자인지 검증
+        int compared = map[x][y];
+        for (int i = x; i < x+n; i++) {
+            for (int j = y; j < y+n; j++) {
+                if (compared != map[i][j]) {
+                    isAvailable = false;
+                } 
+            }
+        }
+        
+        if (isAvailable) {
+            if (compared == -1) {
+                a += 1;
+            }
+            else if (compared == 0) {
+                b += 1;
+            }
+            else {
+                c += 1;
+            }
+        }
+        else {
+            int third = n / 3;
+            // 분할 
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    calculator(x + i * third, y + j * third, third);
+                }
+            }
+        }
+        
+    }
+}
