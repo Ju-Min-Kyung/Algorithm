f3dd8d06e8e13ad3ff8d9f822b81c89eb731fa11
Ju-Min-Kyung <74389635+Ju-Min-Kyung@users.noreply.github.com>
2026-01-12 15:54:42 +0900

    [Silver II] Title: 부분수열의 합, Time: 124 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,45 +1,45 @@
-import java.awt.*;
-import java.io.*;
 import java.util.*;
-import java.util.List;
-import java.util.stream.Stream;
+import java.lang.*;
+import java.io.*;
 
-class Main {
-    static int check = 0;
-    static int S = 0;
-    static int N = 0;
-    static int[] array;
-    public static void main(String[] args) throws IOException {
+class Main
+{
+    static int N, S;
+    static int cnt;
+    static int sum;
+    static int[] arr;
+
+    public static void main(String[] args) throws Exception
+    {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
-        N = Integer.parseInt(line.split(" ")[0]);
-        S = Integer.parseInt(line.split(" ")[1]);
-
-        array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        List<Integer> sequence = new ArrayList<Integer>();
-        backtrack(sequence, 0, 0);
-
-        System.out.println(check);
+        String s = br.readLine();
+        N = Integer.parseInt(s.split(" ")[0]);
+        S = Integer.parseInt(s.split(" ")[1]);
+        arr = new int[N];
+        sum = 0;
+      
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        backTrack(0);
+        System.out.println(cnt);
     }
 
-    public static void backtrack(List<Integer> seq, int start, int depth) {
-        int sum = seq.stream().mapToInt(Integer::intValue).sum();
-        if (seq.size() > 0) {
-            if (sum == S) {
-                check++;
-            }
-        }
-        
-        if (N == seq.size()) {
+    public static void backTrack(int k) {
+        if (k == N) {
             return;
         }
 
-        for (int i = start; i < N; i++) {
-            seq.add(array[i]);
-            backtrack(seq, i+1, depth+1);
-            seq.remove(seq.size()-1);
+        for (int i = k; i < N; i++) {
+
+            sum += arr[i];
+            if (sum == S) {
+                cnt++;
+            }
+            backTrack(i + 1);
+            sum -= arr[i];
+
         }
-
     }
-
 }
