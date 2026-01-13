1e8d0e2deefc09a80ade0e0096f5f490b0cc728c
Ju-Min-Kyung <74389635+Ju-Min-Kyung@users.noreply.github.com>
2026-01-13 21:58:15 +0900

    [Silver II] Title: N과 M (10), Time: 100 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+    static int N, M;
+    static int[] arr;
+    static int[] isUsed;
+    static int[] input;
+
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception
+    {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        input = new int[N];
+        arr = new int[N];
+        isUsed = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(input);
+        backTrack(0,0);
+        System.out.println(sb.toString());
+    }
+
+    static void backTrack(int k, int p) {
+        if (M == k) {
+            for (int i = 0; i < k; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prev = -1;
+        for (int i = p; i < N; i++) {
+            if (input[i] == prev) continue;
+            arr[k] = input[i];
+            prev = input[i];
+            backTrack(k + 1, i + 1);
+
+        }
+    }
+}
