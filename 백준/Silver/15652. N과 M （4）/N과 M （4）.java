989dfa03bb6af60871fdfb0e1e351b1e3d2f7657
Ju-Min-Kyung <74389635+Ju-Min-Kyung@users.noreply.github.com>
2026-01-13 11:55:25 +0900

    [Silver III] Title: N과 M (4), Time: 200 ms, Memory: 19900 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+    static int N, M;
+    static int[] arr;
+    static int[] isUsed;
+    static StringBuilder sb = new StringBuilder();
+	public static void main (String[] args) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+        arr = new int[13];
+        isUsed = new int[13];
+        
+        backTrack(0, 1);
+        System.out.println(sb.toString());
+	}
+	
+	static void backTrack(int k, int p) {
+	    if (M == k) {
+	        for (int i = 0; i < k; i++) {
+	            sb.append(arr[i]).append(" ");
+	        }
+	        sb.append("\n");
+	        return;
+	    }
+	    
+	    for (int i = p; i <= N; i++) {
+	      
+	        arr[k] = i;
+	        backTrack(k+1, i);
+	       
+	    }
+	}
+}
