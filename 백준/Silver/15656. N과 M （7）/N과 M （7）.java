2b752e89fc65db79f0bac6df9371999a009655af
Ju-Min-Kyung <74389635+Ju-Min-Kyung@users.noreply.github.com>
2026-01-13 21:21:20 +0900

    [Silver III] Title: N과 M (7), Time: 524 ms, Memory: 123208 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+    static int N, M;
+    static int[] arr;
+    static int[] isUsed;
+    static int[] input;
+
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception
+    {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        input = new int[N];
+        arr = new int[N];
+        isUsed = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(input);
+        backTrack(0, 0);
+        System.out.println(sb.toString());
+    }
+
+    static void backTrack(int k, int p) {
+        if (M == k) {
+            for (int i = 0; i < k; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            arr[k] = input[i];
+           
+            backTrack(k + 1, i);
+            
+        }
+    }
+}
