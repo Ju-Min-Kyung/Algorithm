f77102721808461ce1a596b29cb2f79d6f707cae
Ju-Min-Kyung <74389635+Ju-Min-Kyung@users.noreply.github.com>
2026-02-02 01:26:14 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 92 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		System.out.println(s.length());
+
+	}
+}
